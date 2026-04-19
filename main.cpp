cb82c0ea39d8f842a4cc068fe1e5afa2750375f6
James Tognola <togdem1@googlemail.com>
2026-04-19 22:40:30 +0100

    ...

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
-#include <iostream>
+#include <print>
+#include <saucer/smartview.hpp>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+coco::stray start(saucer::application *app)
+{
+    auto window  = saucer::window::create(app).value();
+    auto webview = saucer::smartview::create({.window = window});
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    window->set_title("Hello World!");
+
+
+    webview->expose("call_me", [&](double a, double b)
+    {
+        std::println("Called with: a = {}, b = {}", a, b);
+        return a + b;
+    });
+
+
+    webview->expose("call_me_too", [&]() -> coco::task<double>
+    {
+
+        auto random = *co_await webview->evaluate<double>("Math.random()");
+        std::println("Random: {}", random);
+        co_return random;
+    });
+
+    webview->set_url("https://saucer.app");
+
+    window->show();
+
+
+    co_await app->finish();
+}
+
+int main()
+{
+    return saucer::application::create({.id = "hello-world"})->run(start);
 }
